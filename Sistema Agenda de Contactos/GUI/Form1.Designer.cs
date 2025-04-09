072fa919c6da0d8a3c8809046c0a0de467f848e2
Elías Javier Menjívar Ramírez <mr0305032024@unab.edu.sv>
2025-04-08 19:31:52 -0600

    Corrigiendo Comentarios de funcionalidades Javier Menjivar

--- Sistema Agenda de Contactos/GUI/Form1.Designer.cs
+++ Sistema Agenda de Contactos/GUI/Form1.Designer.cs
@@ -2,14 +2,14 @@
 {
     partial class Form1
     {
-        /// <summary>
+        
         /// Variable del diseñador necesaria.
-        /// </summary>
+        
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+
         /// Limpiar los recursos que se estén usando.
-        /// </summary>
+        
         /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
