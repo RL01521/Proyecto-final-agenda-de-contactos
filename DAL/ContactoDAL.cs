d9d9ca9154b7c0699a48f528448774c156bd811c
RL015103 <rl0151032024@unab.edu.sv>
2025-04-22 17:19:50 -0600

    Cambios a DAL y correciones y ignore de git

---
+++ DAL/ContactoDAL.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EL;
+
+namespace DAL
+{
+    public class ContactoDAL
+    {
+        public static void Agregar(Contacto contacto)
+        {
+            using (var db = new ClinicaDentalContext())
+            {
+                db.Contactos.Add(contacto);
+                db.SaveChanges();
+            }
+        }
+
+        public static Contacto ObtenerPorId(int id)
+        {
+            using (var db = new ClinicaDentalContext())
+            {
+                return db.Contactos.Find(id);
+            }
+        }
+
+        public static List<Contacto> ObtenerTodos()
+        {
+            using (var db = new ClinicaDentalContext())
+            {
+                return db.Contactos.ToList();
+            }
+        }
+
+        public static void Actualizar(Contacto contacto)
+        {
+            using (var db = new ClinicaDentalContext())
+            {
+                db.Entry(contacto).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public static void Eliminar(int id)
+        {
+            using (var db = new ClinicaDentalContext())
+            {
+                var contacto = db.Contactos.Find(id);
+                if (contacto != null)
+                {
+                    db.Contactos.Remove(contacto);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
